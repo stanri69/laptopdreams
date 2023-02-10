a75d5c939e2b841fb7b274eabd7c178fe94b6334
stanri69 <stas190602@gmail.com>
2023-02-10 18:13:30 +0200

    created users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome user to the Laptop dreams!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
---
+++ app/models/seller.rb
@@ -0,0 +1,4 @@
+class Seller < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
